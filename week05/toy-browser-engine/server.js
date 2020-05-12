2ae363869a3c9c8ebef5fa23439bc31c171f0f69
xiangchao <vip_319@126.com>
2020-05-12 19:41:44 +0800

    update

--- week05/toy-browser-engine/server.js
+++ week05/toy-browser-engine/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+    // console.log(req)
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('uuid', 'customer123');
+    res.writeHead(200, { 'Content-Type': 'application/json'})
     res.end('{name: "Jon"}');
 });
 
-server.listen('8000');
+server.listen('8000', () => {
+    console.log('server is running.')
+});
