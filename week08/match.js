48d27754c8e91ec51356bf9232e42de34df4a4c1
xiangchao <vip_319@126.com>
2020-06-08 19:37:42 +0800

    finished

--- week08/match.js
+++ week08/match.js
@@ -44,12 +44,58 @@ function getParent(e) {
 // keep thinking
 function getRules(pattern) {
     if (!pattern) return;
+
+    if(pattern.rule) {
+        let item = pattern.rule;
+        let keys = Object.keys(item);
+        let rule = {};
+        keys.forEach( k => {
+            if(k !== 'rule') {
+                rule[k] = item[k]
+            }
+        });
+        rules.push(rule);
+        getRules(item)
+    }
 }
+
+function getMatchCount(targetEle, index) {
+    let matchRule = rules[0];
+
+    if(matchRule.nestingOperator && targetEle.parentElement.tagName === rules[index+1].tagName ) {
+        if(matchRule.tagName && matchRule.tagName === targetEle.tagName) {
+            rules.shift();
+            return 1;
+        }
+
+    }else {
+        if(matchRule.tagName && matchRule.tagName === targetEle.tagName) {
+            rules.shift();
+            return 1;
+        }else {
+            if(matchRule.id === targetEle.id) {
+                rules.shift();
+                return 1;
+            }
+        }
+    }
+   
+
+}
+
 function match(selector, element) {
 
     pattern = parser.parse(selector);
-    
+    getRules(pattern);
+    rules.reverse();
     getParent(element);
+    for(let i = 0; i < parents.length; i++ ) {
+        isMatchCount += getMatchCount(parents[i], i);
+        
+    }
+    console.log(rules)
+
+    return!rules.length;
 }
 
 
