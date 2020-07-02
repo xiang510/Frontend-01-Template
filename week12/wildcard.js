d9bdc2028a7452f1eb9d01249ac13ef72c9043e4
xiang510 <vip_319@126.com>
2020-07-02 21:48:21 +0800

    update

--- week12/wildcard.js
+++ week12/wildcard.js
@@ -35,7 +35,9 @@ function find(source, pattern) {
         let reg = new RegExp(subPattern.replace('/?/g', '[\\s\\S]'), 'g');
 
         reg.lastIndex = lastIndex;
-        reg.exec(source);
+        if (!reg.exec(source)) {
+            return false;
+        }
         lastIndex = reg.lastIndex;
     }
 
