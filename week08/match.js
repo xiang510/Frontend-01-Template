77c657562d0f6dcf729b0d86064a2265db7e9307
xiangchao <vip_319@126.com>
2020-07-29 19:30:38 +0800

    update

--- week08/match.js
+++ week08/match.js
@@ -1,9 +1,9 @@
-var CssSelectorParser = require('css-selector-parser').CssSelectorParser,
-    parser = new CssSelectorParser();
+var CssSelectorParser = require("css-selector-parser").CssSelectorParser,
+  parser = new CssSelectorParser();
 
-parser.registerSelectorPseudos('has');
-parser.registerNestingOperators('>', '+', '~');
-parser.registerAttrEqualityMods('^', '$', '*', '~');
+parser.registerSelectorPseudos("has");
+parser.registerNestingOperators(">", "+", "~");
+parser.registerAttrEqualityMods("^", "$", "*", "~");
 parser.enableSubstitutes();
 
 // console.log(JSON.stringify(parser.parse('#wrap div > a'), false, '    '));
@@ -35,68 +35,65 @@ let parents = [];
 let rules = [];
 
 function getParent(e) {
-    parents.push(e);
-    if (!!e.parentElement) {
-        getParent(e.parentElement);
-    }
+  parents.push(e);
+  if (!!e.parentElement) {
+    getParent(e.parentElement);
+  }
 }
 
 // keep thinking
 function getRules(pattern) {
-    if (!pattern) return;
+  if (!pattern) return;
 
-    if(pattern.rule) {
-        let item = pattern.rule;
-        let keys = Object.keys(item);
-        let rule = {};
-        keys.forEach( k => {
-            if(k !== 'rule') {
-                rule[k] = item[k]
-            }
-        });
-        rules.push(rule);
-        getRules(item)
-    }
+  if (pattern.rule) {
+    let item = pattern.rule;
+    let keys = Object.keys(item);
+    let rule = {};
+    keys.forEach((k) => {
+      if (k !== "rule") {
+        rule[k] = item[k];
+      }
+    });
+    rules.push(rule);
+    getRules(item);
+  }
 }
 
 function getMatchCount(targetEle, index) {
-    let matchRule = rules[0];
+  let matchRule = rules[0];
 
-    if(matchRule.nestingOperator && targetEle.parentElement.tagName === rules[index+1].tagName ) {
-        if(matchRule.tagName && matchRule.tagName === targetEle.tagName) {
-            rules.shift();
-            return 1;
-        }
-
-    }else {
-        if(matchRule.tagName && matchRule.tagName === targetEle.tagName) {
-            rules.shift();
-            return 1;
-        }else {
-            if(matchRule.id === targetEle.id) {
-                rules.shift();
-                return 1;
-            }
-        }
+  if (
+    matchRule.nestingOperator &&
+    targetEle.parentElement.tagName === rules[index + 1].tagName
+  ) {
+    if (matchRule.tagName && matchRule.tagName === targetEle.tagName) {
+      rules.shift();
+      return 1;
     }
-   
-
+  } else {
+    if (matchRule.tagName && matchRule.tagName === targetEle.tagName) {
+      rules.shift();
+      return 1;
+    } else {
+      if (matchRule.id === targetEle.id) {
+        rules.shift();
+        return 1;
+      }
+    }
+  }
 }
 
 function match(selector, element) {
+  pattern = parser.parse(selector);
+  getRules(pattern);
+  rules.reverse();
+  getParent(element);
+  for (let i = 0; i < parents.length; i++) {
+    isMatchCount += getMatchCount(parents[i], i);
+  }
+  console.log(rules);
 
-    pattern = parser.parse(selector);
-    getRules(pattern);
-    rules.reverse();
-    getParent(element);
-    for(let i = 0; i < parents.length; i++ ) {
-        isMatchCount += getMatchCount(parents[i], i);
-        
-    }
-    console.log(rules)
-
-    return!rules.length;
+  return !rules.length;
 }
 
-
-match('#wrap div > a', null)
+match("#wrap div > a", null);
